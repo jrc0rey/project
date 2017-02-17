4d627f12afefb9d5aa5fc8e6b4b02c107caec27f
“JASON” <jrcorey84@gmail.com>
2017-02-17 10:43:22 -0600

    added some functionality to input box and submit button

--- js/script.js
+++ js/script.js
@@ -1,17 +1,41 @@
-console.log('hi')
 
 
 
-$('#startBtn').on('click',function(e){
-	$('#gif').css('visibility', 'visible');
+// $('#startBtn').on('click',function(e){
+// 	$('#gif').show();
+// 	$('#audio').prop('volume','.01');
+// 	$('#audio').trigger('play');
+// setTimeout(function(){
+// 	$('#gif').hide()
+// 	$('#gamePage').css('background-color','black');
+// 	$('body').css('background-color','black');
+// 	$('#pageOne').hide();
+// 	$('#gamePage').css('display', 'flex')
+// 	},3000);
+
+// })
+
+$('#input').keydown(function(event){
+	if(event.keyCode == '13'){
+		$('#submit').click();
+		}
+});
+
+$('#submit').on('click',function(e){                
+	if( $('#input').val() === 'X3478SJ') {
+		$('#access').show()
+        $('#access').text('ACCESS GRANTED')
+	}
+    else{
+    	$('#access').show()
+        $('#access').text('ACCESS DENIED')
+    }
 	setTimeout(function(){
-	$('#gif').css('visibility', 'hidden')
-	$('#gamePage').css('background-color','black');
-	$('#pageOne').remove();
-	$('#gamePage').css('visibility', 'visible')
-	},2000);
+        	$('#access').hide()
+        	$('#input').val('')
+        },2000)
+});
 
-})
 
 
 
