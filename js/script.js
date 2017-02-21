c745bb92065b92f4838fd37a13e8c34539bf66ab
“JASON” <jrcorey84@gmail.com>
2017-02-21 17:02:25 -0600

    added clues one and two,notes box, pictures & icons

--- js/script.js
+++ js/script.js
@@ -1,33 +1,66 @@
 
-//Start Page
+// //Start Page
 
-// $('#startBtn').on('click',function(e){ 
-// 	$('#gif').show();
-// 	$('#audio').prop('volume','.05');
-// 	$('#audio').trigger('play');
-// setTimeout(function(e){
-// 	$('#gif').hide();
-// 	},3000); 
-// 	$('#gamePage').css('display','flex');
-// 	$('#pageOne').css('display', 'none')
-// });
+$('#startBtn').on('click',function(e){ 
+	$('#gif').show();
+	$('#audio').prop('volume','.05');
+	$('#audio').trigger('play');
+setTimeout(function(e){
+	$('#gif').hide();
+	},3000); 
+	$('#gamePage').css('display','flex');
+	$('#pageOne').css('display', 'none')
+});
 
-//Clue One
+// //Clue One
 
 function clueOne(){
-	setInterval(function(){
+	var iconOne = setInterval(function(){
 		$('.picol_rdf').hide();
 	},1000)
 
-	setInterval(function(){
+	var iconTwo = setInterval(function(){
 		$('.picol_rdf').show();
 	},2000)
+
+	$('.picol_rdf').on('click', function(e){
+		$('#overlays').css('display','flex')
+		$('#memo').show();
+	})
+
+	$('#memo').on('click',function(e){
+		clueTwo();
+		$('#overlays').css('display','none')
+		$('.picol_rdf').hide();
+		clearInterval(iconOne)
+		clearInterval(iconTwo)
+	})
 }
+
+function clueTwo(){
+	$('#gridContainer').show();
+	$('.picol_avatar').on('click', function(e){
+		$('#overlays').css('display','flex');
+		$('#howard').show();
+		$('#memo').hide();
+	})
+	$('#howard').on('click',function(e){
+		$('#howard').hide();
+		$('#howard2').show();
+	})
+	$('#howard2').on('click',function(e){
+		$('#howard2').hide();
+		$('#gridContainer').hide();
+		$('#overlays').hide();
+	})
+}
+
+
 
 //Timer
 
 function setTimer(){
-var counter = 50;
+var counter = 75;
 var countdown = setInterval(function(){
 	counter --;
 	if(counter > 0){
@@ -41,6 +74,9 @@ var countdown = setInterval(function(){
 		$('#logo').hide();
 		$('#startBtn').hide();
 		$('#pageOne').css('display', 'flex')
+		$('#reloadBtn').show();
+		$('#siren').prop('volume','.05');
+		$('#siren').trigger('play');
 		};
 	},2000);
 };
@@ -67,9 +103,13 @@ $('#submit').on('click',function(e){
 	}
     else{
     	counter++
-    	$('#attempts').text('attempts: ' + counter + ' of 10');
+    	$('#attempts').text('attempts: ' + counter + ' of 7');
     	$('#access').show();
         $('#access').text('ACCESS DENIED');
+        if ($('#input').val() === 'help.me'){
+        	$('.picol_attachment').show();
+        	$('.picol_attachment').fadeOut(5000);
+        }
         if(counter === 3){
         	$('#access').hide();
         	 setGame();
@@ -77,14 +117,22 @@ $('#submit').on('click',function(e){
         	 $('#clueOne').show();
         	 clueOne();
         	 $('#wrong').hide();
+        	 $('#howardsMark').css('color','red')
+        	 $('#howardsMark').fadeOut(2000)
         	}
-        else if(counter === 10){
+        else if(counter === 4){
+        	$('.picol_attachment').hide();
+        }
+        else if(counter === 7){
         	$('#time').hide();
 			$('#gamePage').css('display', 'none');
 			$('#busted').show();
 			$('#logo').hide();
 			$('#startBtn').hide();
+			$('#reloadBtn').show();
 			$('#pageOne').css('display', 'flex')
+			$('#siren').prop('volume','.05');
+			$('#siren').trigger('play');
         }
        }
 	
@@ -93,6 +141,10 @@ $('#submit').on('click',function(e){
         	$('#input').val('')
         },2000);
 });
+
+$('#reloadBtn').on('click',function(e){
+	location.reload();
+})
 
 
 
